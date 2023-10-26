f7888c607ec9b64f52f925f7c717c7fc83b32baa
刘大大 <you@example.com>
2023-10-26 23:10:54 +0800

    feat: vue中ts的实现

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>hello{{ str }}</div>
+  <div @click="handleClick">hello{{ str }}</div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+// import { greet } from "./index";
+
 const str = "1";
 console.log(str);
 
+const handleClick = () => {
+  // greet({
+  //   name: "string",
+  //   age: 1,
+  // });
+};
 </script>
--- webpack.dev.js
+++ webpack.dev.js
@@ -4,6 +4,8 @@ const { Configuration } = require("webpack");
 const htmlWebpackPlugin = require("html-webpack-plugin");
 const { VueLoaderPlugin } = require("vue-loader/dist/index");
 
+const path = require('path')
+
 /**
  * @type { Configuration } // 使用注解的方式，增加代码智能提示
  */
@@ -17,6 +19,14 @@ const config = {
       {
         test: /\.vue$/, // 解析 .vue 结尾的文件
         use: "vue-loader",
+      },
+      {
+        test: /\.ts$/, // 解析 ts
+        loader: "ts-loader",
+        options: {
+          configFile: path.resolve(process.cwd(), "tsconfig.json"),
+          appendTsSuffixTo: [/\.vue$/],
+        },
       },
     ],
   },
